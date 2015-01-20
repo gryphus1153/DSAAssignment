a88f249af739679a4f3dd4bb15a44f5462ce84d2
gryphus1153 <gryphus1153@hotmail.com>
2015-01-20 14:24:01 +0800

    basic read write done

--- DSAAssignment2014/DSAAssignment2014/Source.cpp
+++ DSAAssignment2014/DSAAssignment2014/Source.cpp
@@ -4,15 +4,28 @@
 
 using namespace std;
 
-vector<Song> Songs;
+vector<Song> songVector;
 int linesLoaded;
+
+void readSong();
 
 void main()
 {
 	cout << "Please enter the number of lines to be loaded";
 	cin >> linesLoaded;
-
+	
 }
 
+void readSongs()
+{
+	int count = 0;
+	do
+	{
+		//read line in
 
-void readIn
+
+
+		count++;
+		cout << 
+	} while (count < linesLoaded);
+}
